16773a0eb94a1be72408777e3df7073e65e3ff19
Maciej Kopeć <maciejkopec92@gmail.com>
2025-12-10 15:27:21 +0100

    Update to v0.1.32

--- Formula/vi-mongo.rb
+++ Formula/vi-mongo.rb
@@ -1,30 +1,30 @@
 class ViMongo < Formula
   desc "Terminal User Interface for MongoDB"
   homepage "https://github.com/kopecmaciej/vi-mongo"
-  version "v0.1.31"
+  version "v0.1.32"
 
   # Checksums for different platforms
-  MACOS_ARM64_SHA256 = "dc3d5d2c43663e25a0afcf677ff4af26bb6dc5cd317d6cc134290ba13b1fe7d3"
-  MACOS_X86_64_SHA256 = "708d0232d0392d5951c28f9f8103fc2d367d0bd790cc21d7c67e24fee2a6de19"
-  LINUX_ARM64_SHA256 = "5c1263f332bfb0e3200486abc00a271d26909f9a34504f7032b21c1f2f6f0bbb"
-  LINUX_X86_64_SHA256 = "4afe6899e25af93552792d59bb87c9d1c10af665656b81b61815fa74d12a0d1c"
+  MACOS_ARM64_SHA256 = "d479f75306e79ee3077755b72960d30ad7267e3378f93379f457b3e66554f3be"
+  MACOS_X86_64_SHA256 = "209626d2acd751d60767fa4058bd1024c9ad5f416ab06c12f992e0248b107605"
+  LINUX_ARM64_SHA256 = "5cc4d55bf11800131656eaa3a94a0e9c539bfb26215188dfcc5cb455e59652d6"
+  LINUX_X86_64_SHA256 = "962fdbce89f621db8a4db922da62c343242ff2d7a8f7209eede1be72561cedc1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/kopecmaciej/vi-mongo/releases/download/v0.1.31/vi-mongo_Darwin_arm64.tar.gz"
+      url "https://github.com/kopecmaciej/vi-mongo/releases/download/v0.1.32/vi-mongo_Darwin_arm64.tar.gz"
       sha256 MACOS_ARM64_SHA256
     else
-      url "https://github.com/kopecmaciej/vi-mongo/releases/download/v0.1.31/vi-mongo_Darwin_x86_64.tar.gz"
+      url "https://github.com/kopecmaciej/vi-mongo/releases/download/v0.1.32/vi-mongo_Darwin_x86_64.tar.gz"
       sha256 MACOS_X86_64_SHA256
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/kopecmaciej/vi-mongo/releases/download/v0.1.31/vi-mongo_Linux_arm64.tar.gz"
+      url "https://github.com/kopecmaciej/vi-mongo/releases/download/v0.1.32/vi-mongo_Linux_arm64.tar.gz"
       sha256 LINUX_ARM64_SHA256
     else
-      url "https://github.com/kopecmaciej/vi-mongo/releases/download/v0.1.31/vi-mongo_Linux_x86_64.tar.gz"
+      url "https://github.com/kopecmaciej/vi-mongo/releases/download/v0.1.32/vi-mongo_Linux_x86_64.tar.gz"
       sha256 LINUX_X86_64_SHA256
     end
   end
